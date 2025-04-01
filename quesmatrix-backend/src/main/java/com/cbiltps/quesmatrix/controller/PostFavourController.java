7e79ef70830309a9b7518c9db67a7b77556c4620
lichengxiang <cbiltps@yeah.net>
2025-04-01 10:11:22 +0800

    后端项目初始化以及后端模版开发

---
+++ quesmatrix-backend/src/main/java/com/cbiltps/quesmatrix/controller/PostFavourController.java
@@ -0,0 +1,109 @@
+package com.cbiltps.quesmatrix.controller;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.cbiltps.quesmatrix.common.BaseResponse;
+import com.cbiltps.quesmatrix.common.ErrorCode;
+import com.cbiltps.quesmatrix.common.ResultUtils;
+import com.cbiltps.quesmatrix.exception.BusinessException;
+import com.cbiltps.quesmatrix.exception.ThrowUtils;
+import com.cbiltps.quesmatrix.model.dto.post.PostQueryRequest;
+import com.cbiltps.quesmatrix.model.dto.postfavour.PostFavourAddRequest;
+import com.cbiltps.quesmatrix.model.dto.postfavour.PostFavourQueryRequest;
+import com.cbiltps.quesmatrix.model.entity.Post;
+import com.cbiltps.quesmatrix.model.entity.User;
+import com.cbiltps.quesmatrix.model.vo.PostVO;
+import com.cbiltps.quesmatrix.service.PostFavourService;
+import com.cbiltps.quesmatrix.service.PostService;
+import com.cbiltps.quesmatrix.service.UserService;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 帖子收藏接口
+ *
+ * @author 
+ * @from 
+ */
+@RestController
+@RequestMapping("/post_favour")
+@Slf4j
+public class PostFavourController {
+
+    @Resource
+    private PostFavourService postFavourService;
+
+    @Resource
+    private PostService postService;
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 收藏 / 取消收藏
+     *
+     * @param postFavourAddRequest
+     * @param request
+     * @return resultNum 收藏变化数
+     */
+    @PostMapping("/")
+    public BaseResponse<Integer> doPostFavour(@RequestBody PostFavourAddRequest postFavourAddRequest,
+            HttpServletRequest request) {
+        if (postFavourAddRequest == null || postFavourAddRequest.getPostId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能操作
+        final User loginUser = userService.getLoginUser(request);
+        long postId = postFavourAddRequest.getPostId();
+        int result = postFavourService.doPostFavour(postId, loginUser);
+        return ResultUtils.success(result);
+    }
+
+    /**
+     * 获取我收藏的帖子列表
+     *
+     * @param postQueryRequest
+     * @param request
+     */
+    @PostMapping("/my/list/page")
+    public BaseResponse<Page<PostVO>> listMyFavourPostByPage(@RequestBody PostQueryRequest postQueryRequest,
+            HttpServletRequest request) {
+        if (postQueryRequest == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        User loginUser = userService.getLoginUser(request);
+        long current = postQueryRequest.getCurrent();
+        long size = postQueryRequest.getPageSize();
+        // 限制爬虫
+        ThrowUtils.throwIf(size > 20, ErrorCode.PARAMS_ERROR);
+        Page<Post> postPage = postFavourService.listFavourPostByPage(new Page<>(current, size),
+                postService.getQueryWrapper(postQueryRequest), loginUser.getId());
+        return ResultUtils.success(postService.getPostVOPage(postPage, request));
+    }
+
+    /**
+     * 获取用户收藏的帖子列表
+     *
+     * @param postFavourQueryRequest
+     * @param request
+     */
+    @PostMapping("/list/page")
+    public BaseResponse<Page<PostVO>> listFavourPostByPage(@RequestBody PostFavourQueryRequest postFavourQueryRequest,
+            HttpServletRequest request) {
+        if (postFavourQueryRequest == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        long current = postFavourQueryRequest.getCurrent();
+        long size = postFavourQueryRequest.getPageSize();
+        Long userId = postFavourQueryRequest.getUserId();
+        // 限制爬虫
+        ThrowUtils.throwIf(size > 20 || userId == null, ErrorCode.PARAMS_ERROR);
+        Page<Post> postPage = postFavourService.listFavourPostByPage(new Page<>(current, size),
+                postService.getQueryWrapper(postFavourQueryRequest.getPostQueryRequest()), userId);
+        return ResultUtils.success(postService.getPostVOPage(postPage, request));
+    }
+}
---
+++ quesmatrix-backend/src/main/java/com/cbiltps/quesmatrix/mapper/AppMapper.java
@@ -0,0 +1,18 @@
+package com.cbiltps.quesmatrix.mapper;
+
+import com.cbiltps.quesmatrix.model.entity.App;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 
+* @description 针对表【app(应用)】的数据库操作Mapper
+* @createDate 2024-05-09 20:41:03
+* @Entity com.cbiltps.quesmatrix.model.entity.App
+*/
+public interface AppMapper extends BaseMapper<App> {
+
+}
+
+
+
+
