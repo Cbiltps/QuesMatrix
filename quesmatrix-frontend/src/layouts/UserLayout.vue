04b72d8313a07a356b661c678fd28fb656508ab2
lichengxiang <cbiltps@yeah.net>
2025-04-04 19:05:58 +0800

    添加管理页面

--- quesmatrix-frontend/src/layouts/UserLayout.vue
+++ quesmatrix-frontend/src/layouts/UserLayout.vue
@@ -4,7 +4,7 @@
       <a-layout-header class="header">
         <a-space>
           <img class="logo" src="../assets/logo.png" />
-          <div>题域矩阵 AI 答题应用平台</div>
+          <div>题域矩阵 AI答题应用平台</div>
         </a-space>
       </a-layout-header>
       <a-layout-content class="content">
