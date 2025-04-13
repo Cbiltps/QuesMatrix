313c6ccf45a927d2f62cc2e3adeae9ceca432a1d
lichengxiang <cbiltps@yeah.net>
2025-04-14 05:54:30 +0800

    添加幂等设计-复用主键方案

--- quesmatrix-backend/src/main/java/com/cbiltps/quesmatrix/model/dto/userAnswer/UserAnswerAddRequest.java
+++ quesmatrix-backend/src/main/java/com/cbiltps/quesmatrix/model/dto/userAnswer/UserAnswerAddRequest.java
@@ -1,7 +1,5 @@
 package com.cbiltps.quesmatrix.model.dto.userAnswer;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -15,6 +13,11 @@ import java.util.List;
  */
 @Data
 public class UserAnswerAddRequest implements Serializable {
+
+    /**
+     * 用户答案 id 用于保证提交答案的幂等性
+     */
+    private Long id;
 
     /**
      * 应用 id
